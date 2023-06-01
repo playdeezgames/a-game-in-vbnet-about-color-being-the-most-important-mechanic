b51af5c60b5430508ea5b98efd0992e85ee42192
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-01 05:46:38 -0500

    shipped it!

--- shippit.sh
+++ shippit.sh
@@ -1,8 +1,8 @@
-dotnet publish ./src/Doomhawker/Doomhawker.vbproj -o ./pub-linux -c Release --sc -r linux-x64
-dotnet publish ./src/Doomhawker/Doomhawker.vbproj -o ./pub-windows -c Release --sc -r win-x64
-dotnet publish ./src/Doomhawker/Doomhawker.vbproj -o ./pub-mac -c Release --sc -r osx-x64
-butler push pub-windows thegrumpygamedev/doomhawker:windows
-butler push pub-linux thegrumpygamedev/doomhawker:linux
-butler push pub-mac thegrumpygamedev/doomhawker:mac
+dotnet publish ./src/AGameInVbNetAboutColorBeingTheMostImportantMechanic/AGameInVbNetAboutColorBeingTheMostImportantMechanic.vbproj -o ./pub-linux -c Release --sc -r linux-x64
+dotnet publish ./src/AGameInVbNetAboutColorBeingTheMostImportantMechanic/AGameInVbNetAboutColorBeingTheMostImportantMechanic.vbproj -o ./pub-windows -c Release --sc -r win-x64
+dotnet publish ./src/AGameInVbNetAboutColorBeingTheMostImportantMechanic/AGameInVbNetAboutColorBeingTheMostImportantMechanic.vbproj -o ./pub-mac -c Release --sc -r osx-x64
+butler push pub-windows thegrumpygamedev/a-game-in-vbnet-about-color-being-the-most-important-mechanic:windows
+butler push pub-linux thegrumpygamedev/a-game-in-vbnet-about-color-being-the-most-important-mechanic:linux
+butler push pub-mac thegrumpygamedev/a-game-in-vbnet-about-color-being-the-most-important-mechanic:mac
 git add -A
 git commit -m "shipped it!"
