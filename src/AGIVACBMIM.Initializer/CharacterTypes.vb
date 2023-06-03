37279195b4273c9b39e881769613565ff835b9d0
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-03 04:54:16 -0500

    added the other mechanics

--- src/AGIVACBMIM.Initializer/CharacterTypes.vb
+++ src/AGIVACBMIM.Initializer/CharacterTypes.vb
@@ -3,7 +3,12 @@
         New Dictionary(Of String, CharacterTypeDescriptor) From
         {
             {N00b, New CharacterTypeDescriptor("N00b", spawnCount:=1)},
-            {Color, New CharacterTypeDescriptor("Color, the most important mechanic", spawnCount:=1)}
+            {Color, New CharacterTypeDescriptor("Color, the most important mechanic", spawnCount:=1)},
+            {Hue, New CharacterTypeDescriptor("Hue, the mechanic", spawnCount:=1)},
+            {Shade, New CharacterTypeDescriptor("Shade, the mechanic", spawnCount:=1)},
+            {Tint, New CharacterTypeDescriptor("Tint, the mechanic", spawnCount:=1)},
+            {Tone, New CharacterTypeDescriptor("Tone, the mechanic", spawnCount:=1)},
+            {Pigment, New CharacterTypeDescriptor("Pigment, the mechanic", spawnCount:=1)}
         }
     Friend ReadOnly Property All As IEnumerable(Of String)
         Get
@@ -12,4 +17,9 @@
     End Property
     Friend Const N00b = "N00b"
     Friend Const Color = "Color"
+    Friend Const Hue = "Hue"
+    Friend Const Shade = "Shade"
+    Friend Const Tint = "Tint"
+    Friend Const Tone = "Tone"
+    Friend Const Pigment = "Pigment"
 End Module
