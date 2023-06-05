cc591889c9385d969896989e9ed99ef3abbfe00c
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-05 04:37:34 -0500

    i can look at my inventory

--- src/AGIVACBMIM.Application/Inventory.vb
+++ src/AGIVACBMIM.Application/Inventory.vb
@@ -11,6 +11,39 @@
             Case NeverMindText
                 Return
             Case Else
+                RunItemStack(table(answer))
+        End Select
+    End Sub
+    Private Sub RunItemStack(items As IEnumerable(Of IItem))
+        If items.Count = 1 Then
+            RunItem(items.Single)
+            Return
+        End If
+        Dim prompt As New SelectionPrompt(Of String) With {.Title = $"[olive]Which {items.First.Name}?[/]"}
+        prompt.AddChoice(NeverMindText)
+        Dim suffix = ""
+        Dim table As New Dictionary(Of String, IItem)
+        For Each item In items
+            table($"{item.Name}{suffix}") = item
+            suffix &= " " + ChrW(8)
+        Next
+        prompt.AddChoices(table.Keys)
+        Dim answer = AnsiConsole.Prompt(prompt)
+        Select Case answer
+            Case NeverMindText
+                Return
+            Case Else
+                RunItem(table(answer))
+        End Select
+    End Sub
+
+    Private Sub RunItem(item As IItem)
+        Dim prompt As New SelectionPrompt(Of String) With {.Title = $"[olive]{item.Name}[/]"}
+        prompt.AddChoice(NeverMindText)
+        Select Case AnsiConsole.Prompt(prompt)
+            Case NeverMindText
+                Return
+            Case Else
                 Throw New NotImplementedException
         End Select
     End Sub
