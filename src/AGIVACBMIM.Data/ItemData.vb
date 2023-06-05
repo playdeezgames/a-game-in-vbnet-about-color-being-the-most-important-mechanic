b636a80701f1184b2b2dadfa7d7a8b9f4a063b7b
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-05 03:40:04 -0500

    rearrange for great justice

---
+++ src/AGIVACBMIM.Data/ItemData.vb
@@ -0,0 +1,4 @@
+﻿Public Class ItemData
+    Public Property ItemType As String
+    Public Property Name As String
+End Class
---
+++ src/AGIVACBMIM.Initializer/Items/ItemTypes.vb
@@ -0,0 +1,11 @@
+﻿Friend Module ItemTypes
+    Friend ReadOnly Descriptors As IReadOnlyDictionary(Of String, ItemTypeDescriptor) =
+        New Dictionary(Of String, ItemTypeDescriptor) From
+        {
+            {
+                Snax,
+                New ItemTypeDescriptor("Snax")
+            }
+        }
+    Friend Const Snax = "Snax"
+End Module
